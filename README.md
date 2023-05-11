e8159ec9d1774d12e35d82a7873d6bc27d16677d
CyberRex <hspwinx86@gmail.com>
2023-05-12 00:11:37 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# summaly-server
+
+```
+pnpm i
+pnpm start
+```
+
+You can set listen port manually via `PORT` enviroment variable.
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import summaly from 'summaly';
+import privateip from 'private-ip';
+const app = express();
+const port = process.env.PORT || 3000;
+
+console.log(summaly);
+
+const asyncWrapper = fn => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next);
+    };
+}
+
+app.get('/', asyncWrapper(async (req, res) => {
+    const url = req.query.url;
+    if (!url) {
+        res.statusCode = 400;
+        res.end('Please provide a URL');
+        return;
+    }
+    let urlInfo = null;
+    try {
+        urlInfo = new URL(url);
+    } catch (e) {
+        console.log(e.stack);
+        res.statusCode = 400;
+        res.end('Invalid URL');
+        return;
+    }
+    if (urlInfo.protocol !== 'http:' && urlInfo.protocol !== 'https:') {
+        res.statusCode = 400;
+        res.end('Only HTTP and HTTPS are supported');
+        return;
+    }
+    if (await privateip(urlInfo.hostname)) {
+        res.statusCode = 400;
+        res.end('Forbidden');
+        return;
+    }
+
+    try {
+        const summary = await summaly.default(url);
+        res.statusCode = 200;
+        res.json(summary);
+    } catch (e) {
+        console.log(e);
+        console.log(e.stack);
+    }
+
+}));
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
